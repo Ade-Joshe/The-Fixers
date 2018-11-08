d801108d53e7162925ce8373c1211637e7b56332
Igwe Augustine <augustusbest00@gmail.com>
2018-11-08 01:42:22 -0900

    edited app.js, package.json and server.js

--- app.js
+++ app.js
@@ -55,4 +55,8 @@ app.use((error, req, res, next) => {
     });
 })
 
-module.exports = app;
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+});
